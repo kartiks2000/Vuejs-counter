4f3340b43c5480c897a6e22ab41fdad2dfd1b27f
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-18 12:46:41 +0530

    Add files via upload

---
+++ src/child1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>child counter: {{count}}</h3>
+    <button @click="inc1">Increment</button>
+    <button @click="dec1">Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["count"],
+  methods:{
+      inc1(){
+          this.$emit("incit",1);
+      },
+      dec1(){
+          this.$emit("decit",1);
+      }
+  }
+}
+</script>
+
+<style scoped>
+    div{
+        text-align: center;
+    }
+</style>
